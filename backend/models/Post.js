7bf6b52438ae0474da5e40c8fb8b17dc20a66bb6
maulana saputra <maulana.saputra@aqi.co.id>
2022-09-13 09:58:28 +0700

    clone devconnector_2.0
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId
+  },
   text: {
     type: String,
     required: true
@@ -12,6 +15,34 @@ const PostSchema = new Schema({
   avatar: {
     type: String
   },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
   date: {
     type: Date,
     default: Date.now
