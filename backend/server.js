e80b3d0572513b17e943deb40a8ce3c22740ec93
maulana saputra <maulana.saputra@aqi.co.id>
2022-09-13 09:58:28 +0700

    clone devconnector_2.0
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ db.open();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
